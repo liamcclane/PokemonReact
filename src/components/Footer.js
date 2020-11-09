7a09a48e19feff38568dfa2cb16ec74f671c533f
liamcclane <lia_mcclane@msn.com>
2020-11-09 13:31:08 -0800

    completed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import CSSClasses from './Footer.module.css';
 
+import World from './../assets/images/icons/world-icon.png';
 
 export default props => {
 
@@ -25,15 +26,38 @@ export default props => {
         "Speed Test",
         "Legal Notices",
         "Netflix Originals"
-    ])
+    ]);
+    const [helpNumber, setHelpNumber] = useState({
+        wordNum: '1-800-HELP-ME!',
+        numNum: '1-800-435-7631'
+    });
+
+    let buttonStyle = {
+        "marginTop":"5px"
+    };
 
     return (
-        <div className="container-lg container-fluid-sm">
-            <ul className={CSSClasses.footerList}>
-                {footerLinks.map((ele, i) => (
-                    <li className={CSSClasses.footerTags} key={i} >{ele}</li>
-                ))}
-            </ul>
+        <div className="container-fluid" id={CSSClasses.Footer}>
+            <div className="container-lg container-fluid-sm">
+                <p>Questions? Call {helpNumber.wordNum}  or  {helpNumber.numNum}</p>
+                <ul className={CSSClasses.footerList}>
+                    {footerLinks.map((ele, i) => (
+                        <li className={CSSClasses.footerTags} key={i} >{ele}</li>
+                    ))}
+                </ul>
+                <div className="dropdown" style={buttonStyle}>
+                    <button className="btn btn-outline-secondary dropdown-toggle"
+                        type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src={World} alt="world" class={CSSClasses.worldFooter}></img>
+                    English
+                </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a className="dropdown-item" href="/spanish">Spanish</a>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
