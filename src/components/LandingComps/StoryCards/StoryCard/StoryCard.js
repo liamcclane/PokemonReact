be2f3dc92334cd86953793d0fa71b8948430a48d
liamcclane <lia_mcclane@msn.com>
2020-11-12 15:06:24 -0800

    dynamic story card outputs

--- src/components/LandingComps/StoryCards/StoryCard/StoryCard.js
+++ src/components/LandingComps/StoryCards/StoryCard/StoryCard.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
+// importing other components
 
-export default ({ info, someImg }) => {
+// importing stylesheets
+import CSSClasses from './StoryCard.module.css';
+
+// importing static content
+
+
+
+export default ({ header, underText, img }) => {
+
+    let imgClasses = ["img-thumbnail", CSSClasses.img];
+    imgClasses = imgClasses.join(" ");
+
 
     return (
         <>
-            <div className="col-3">
-
+            <div className="col-5">
+                <h1>{header}</h1>
+                <p>{underText}</p>
             </div>
-            <img src={someImg} alt="img"></img>
+            <div className="col-5">
+                <img src={img} alt="img" className={imgClasses}></img>
+            </div>
         </>
     );
 }
--- src/components/LandingComps/StoryCards/StoryCards.js
+++ src/components/LandingComps/StoryCards/StoryCards.js
@@ -10,9 +10,27 @@ import SomeImg from './../../../assets/images/posters_2.jpg';
 export default props => {
 
     const [stories, setStories] = useState([
-        { "somekindakey": "somekindValue1" },
-        { "somekindakey": "somekindValue2" },
-        { "somekindakey": "somekindValue3" }
+        {
+            "somekindakey": "somekindValue1",
+            "header" :"Enjoy on your TV",
+            "underText":"Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+            "img" : SomeImg,
+
+        },
+        {
+            "somekindakey": "somekindValue2",
+            "header" :"Download your shows to watch offline.",
+            "underText":"Save your favorites easily and always have something to watch.",
+            "img" : SomeImg,
+
+        },
+        {
+            "somekindakey": "somekindValue3",
+            "header" :"Watch everywhere.",
+            "underText":" Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.",
+            "img" : SomeImg,
+
+        }
     ])
 
 
@@ -23,14 +41,14 @@ export default props => {
             <ul className={CSSClasses.list}>
                 {stories.map((ele, ind) => {
                     let classes = [CSSClasses.cardContainer];
-                    if(ind%2==1) {
+                    if (ind % 2 == 1) {
                         classes.push(CSSClasses.cardContainerReverse);
                     }
                     classes = classes.join(' ');
                     return (
-                    <li key={ind} className={classes}>
-                        <StoryCard info={ele.somekindakey} someImg={SomeImg}/>
-                    </li>)
+                        <li key={ind} className={classes}>
+                            <StoryCard header={ele.header} underText={ele.underText} img={ele.img} />
+                        </li>)
                 })}
                 <li className={CSSClasses.cardContainer}>
                     <FAQComp />
