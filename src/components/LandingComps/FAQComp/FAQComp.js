aacefbcde51031de0057a3b8847d55fc4fbedde9
liamcclane <lia_mcclane@msn.com>
2020-11-12 15:05:40 -0800

    finished FAQ accordion

--- src/components/LandingComps/FAQComp/FAQComp.js
+++ src/components/LandingComps/FAQComp/FAQComp.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // importing other components
+import FAQItem from './FAQItems/FAQItem';
+
 // importing style sheets
+import CSSClasses from './FAQComp.module.css';
+
 // importing static content
 
 export default props => {
+
+    const [stories, setStories] = useState([
+        {
+            "title": "What is What With Me?",
+            "isOpen":false,
+            "information": ["Watch With Me, is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.", 
+            "You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!"]
+        },
+        {
+            "title": "How much does Watch With Me Cost?",
+            "isOpen":false,
+            "information": ["Watch With Me is completely free! Use it to create fun list that you and your friends to binge though"]
+        },
+        {
+            "title": "Where can I access Watch With Me?",
+            "isOpen":false,
+            "information": ["With out App, you can log onto you account any time you have wifi or are connected to the internet."]
+        },
+        {
+            "title": "How do I cancel?",
+            "isOpen":false,
+            "information": ["There is no canceling needed! This this is complete free application. Tell all your friends about it!"]
+        },
+        {
+            "title": "What does Watch With Me include?",
+            "isOpen":false,
+            "information": ["Watch With Me has an unlimited number of list you can create then share again with your friends. Then as you update the list and check off shows you have watched together, everyone can see a fun history of the shows and movie you have consumed together."]
+        },
+    ]);
+
+    const openHandler = (e, ind) => {
+        let bool = stories[ind]['isOpen'];
+        // copied
+        let arr = [...stories];
+        // altered
+        arr[ind]["isOpen"] = !bool;
+        // replaced back into setState
+        setStories(arr);
+    }
+
+
     return (
-        <>
-        FAQ
-        </>
+        <div className="col-9">
+            <div className={CSSClasses.myAccordian} id="accordion">
+                {stories.map((story, ind) => (
+                    <FAQItem key={ind} ind={ind} title={story.title} 
+                    isOpen={story.isOpen} information={story.information} 
+                    openHandler={openHandler}/>
+                ))}
+            </div>
+        </div>
     );
 };
---
+++ src/components/LandingComps/FAQComp/FAQItems/FAQItem.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+// importing other components
+// importing style sheets
+import CSSClasses from './FAQItem.module.css';
+// importing static content
+import Plus from './../../../../assets/images/icons/plus-sm.png';
+import EX from './../../../../assets/images/icons/x_icon.png';
+
+export default ({ title, information, ind, openHandler, isOpen }) => {
+
+    let cardClasses = ["card", "mb-2", CSSClasses.myCard];
+    let titleClasses = ["card-header", CSSClasses.title];
+    let cardBodyClasses = ["card-body", CSSClasses.cardBody];
+   
+    cardClasses = cardClasses.join(" ");
+    titleClasses = titleClasses.join(" ");
+    cardBodyClasses = cardBodyClasses.join(" ");
+   
+    return (
+        <div className={cardClasses}>
+            <div className={titleClasses} id={"heading" + ind} >
+                <div className="mb-0 row align-items-center justify-content-between"
+                    data-toggle="collapse" data-target={"#title" + ind} aria-expanded="false"
+                    aria-controls={"title" + ind} onClick={(e) => openHandler(e, ind)}>
+                    <span className="ml-3">
+                        {title}
+                    </span>
+                    {isOpen
+                        ? <img className={CSSClasses.iconImg} alt="close" src={EX} ></img>
+                        : <img className={CSSClasses.iconImg} alt="open" src={Plus} ></img>
+                    }
+                </div>
+            </div>
+
+            <div id={"title" + ind} className="collapse" aria-labelledby={"heading" + ind} data-parent="#accordion">
+                <div className={cardBodyClasses}>
+                    {information.map((ele, i) => (
+                        <p ind={i}>{ele}</p>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
