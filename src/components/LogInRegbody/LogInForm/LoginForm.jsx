139c58e7475d60a3eef7f8e988e9623f8b85ce4d
liamcclane <lia_mcclane@msn.com>
2020-12-09 15:24:02 -0800

    styling loginForm component

--- src/components/LogInRegbody/LogInForm/LoginForm.jsx
+++ src/components/LogInRegbody/LogInForm/LoginForm.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
-import {Link} from '@reach/router';
+import { Link } from '@reach/router';
 
-export default ({email, changey, signIn}) => {
+export default ({ email, changey, signIn }) => {
     return (<>
-        <h1>LOGIN</h1>
+        <h1>Welcome back, Sign In</h1>
         <form onSubmit={e => signIn(e, email)}>
-            <input type="email" name="email"
-                placeholder="email@mail.com"
-                onChange={e => changey(e)} />
-            <input type="submit" className="btn btn-danger" />
-            <Link className="text-danger" to="/register">create an account</Link>
+            <div className="input-group mb-3 mt-4">
+                <div className="input-group-prepend">
+                    <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
+                </div>
+                <input className="form-control" aria-label="Default"
+                    aria-describedby="inputGroup-sizing-default" type="email" name="email"
+                    placeholder="email@email.com"
+                    onChange={e => changey(e)} />
+            </div>
+            <div className="row p-3 justify-content-between">
+                <input type="submit" className="btn btn-danger" />
+                <Link className="text-danger" to="/register">create an account</Link>
+            </div>
         </form>
     </>)
 }
